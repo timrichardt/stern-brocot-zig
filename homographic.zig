c23e7f1a0bc4d32ca9ff2566fa479c689b93d0dd
Tim Richardt <tim@richardt.io>
2025-02-20 16:10:16 +0100

    refactor: union type for signsum

--- homographic.zig
+++ homographic.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const tree = @import("tree.zig");
 
+const SignSum = union(enum) {
+    value: i32,
+    undefined: bool,
+};
+
 pub fn sgn(a: i64) i32 {
     if (a == 0) {
         return 0;
@@ -11,18 +16,18 @@ pub fn sgn(a: i64) i32 {
     }
 }
 
-pub fn linSgn(a: i64, b: i64) i32 {
+pub fn linSgn(a: i64, b: i64) SignSum {
     if (a == 0 and b == 0) {
-        return 0;
+        return SignSum{ .value = 0 };
     } else {
         const sumOfSigns = sgn(a) + sgn(b);
 
         if (sumOfSigns > 0) {
-            return 1;
+            return SignSum{ .value = 1 };
         } else if (sumOfSigns < 0) {
-            return -1;
+            return SignSum{ .value = -1 };
         } else {
-            return -2;
+            return SignSum{ .undefined = true };
         }
     }
 }
@@ -34,8 +39,8 @@ pub fn homSgn(H: tree.Node, u: []const u8) !i32 {
         const nomSgn = linSgn(H.a, H.b);
         const denomSgn = linSgn(H.c, H.d);
 
-        if (nomSgn > -2 and denomSgn > -2) {
-            return nomSgn * denomSgn;
+        if (nomSgn != .undefined and denomSgn != .undefined) {
+            return nomSgn.value * denomSgn.value;
         } else {
             const head = u[0];
             const rest = u[1..];
