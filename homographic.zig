05bba585d0e8dcdf772da14f79ed9d15bd002a84
Tim Richardt <tim@richardt.io>
2025-02-21 16:15:40 +0100

    test: add test for large sequences

--- homographic.zig
+++ homographic.zig
@@ -54,17 +54,31 @@ test "Homographic sign algorithm" {
     const node = tree.Node{ .a = 5, .b = -7, .c = 1, .d = 2 };
     const u = "RLLRLR";
 
-    try std.testing.expect(try homSgn(node, u) == -1);
+    try std.testing.expect(homSgn(node, u) == -1);
+}
+
+test "Homographic sign algorithm for large number" {
+    const node = tree.Node{ .a = 1, .b = -10_000_001, .c = 1, .d = 0 };
+
+    var buffer: [10_000_000]u8 = undefined;
+
+    for (0..10_000_000) |i| {
+        buffer[i] = 'R';
+    }
+
+    // Convert buffer to slice for printing or further use
+    const u = buffer[0..];
+    try std.testing.expect(homSgn(node, u) == 0);
 }
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    const node = tree.Node{ .a = 1, .b = -5000002, .c = 1, .d = 0 };
+    const node = tree.Node{ .a = 1, .b = -10_000_000, .c = 1, .d = 0 };
 
-    var buffer: [5000000]u8 = undefined;
+    var buffer: [10_000_000]u8 = undefined;
 
-    for (0..5000000) |i| {
+    for (0..10_000_000) |i| {
         buffer[i] = 'R';
     }
 
